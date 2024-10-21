4d67c3a9df746033bc1f1cd583d2d988a00301f0
JoaoVitorPaludo <joaovpaludo@gmail.com>
2024-10-21 09:30:24 -0300

    feat: Implementando a tela de informações pessoais e download de currículo

--- src/components/aboutMe/index.tsx
+++ src/components/aboutMe/index.tsx
@@ -1,25 +1,75 @@
+import { Fade, Grow, Slide, Zoom } from '@mui/material'
+import { DownloadSimple } from '@phosphor-icons/react'
 import image from '../../assets/profileImage.jpeg'
+import { useVisibilityObserver } from '../../utils/intersectionObserver'
 import * as S from './styles'
 
 export function AboutMe() {
+  const { isVisible, elementRef } = useVisibilityObserver(0.1)
+
   return (
-    <S.AboutMeContainer>
+    <S.AboutMeContainer ref={elementRef}>
       <h3>SOBRE MIM</h3>
       <S.AboutMeMainCard>
-        <img src={image} alt="" />
+        <Grow in={isVisible} timeout={2000}>
+          <img src={image} alt="" />
+        </Grow>
+        <aside>
+          <Slide in={isVisible} timeout={2000} direction="left">
+            <p>
+              Olá, sou João Vitor, tenho 22 anos e sou formado em Análise e
+              Desenvolvimento de Sistemas pela Universidade de Passo Fundo
+              (UPF). Atualmente, resido em Paraí/RS e atuo como desenvolvedor de
+              software desde agosto de 2022, com uma paixão especial por
+              desenvolvimento web.
+              <br />
+              Estou sempre em busca de aprimorar meus conhecimentos, tanto
+              pessoais quanto profissionais, e adoro enfrentar novos desafios.
+              Sou uma pessoa proativa, comprometida e tenho facilidade para
+              trabalhar em equipe. Além disso, tenho experiência em liderar
+              projetos, o que me motiva a entregar soluções inovadoras e
+              eficientes.
+            </p>
+          </Slide>
 
-        <p>
-          Me chamo João Vitor, tenho 22 anos e sou formado em Analise e
-          Desenvolvimento de Sistemas pela UPF (Universidade de Passo Fundo).
-          <br />
-          Atualmente resido na cidade de Paraí/RS, trabalho com desenvolvimento
-          de software desde 08/2022, sou apaixonado por programação
-          especialmente por desenvolvimento web. <br />
-          Busco sempre agregar ainda mais meus conhecimentos tanto pessoais como
-          profissionais e anseio por novos desafios.
-          <br /> Me considero uma pessoa proativa e leal, gosto de trabalhar em
-          equipe e atuar na liderança de projetos.
-        </p>
+          <S.AboutMeSkills>
+            <Zoom in={isVisible} timeout={1500}>
+              <img
+                alt="react"
+                src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg"
+              />
+            </Zoom>
+            <Zoom in={isVisible} timeout={1500}>
+              <img
+                alt="javascript"
+                src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-plain.svg"
+              />
+            </Zoom>
+            <Zoom in={isVisible} timeout={1500}>
+              <img
+                alt="typescript"
+                src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-plain.svg"
+              />
+            </Zoom>
+            <Zoom in={isVisible} timeout={1500}>
+              <img
+                alt="typescript"
+                src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/csharp/csharp-original.svg"
+              />
+            </Zoom>
+
+            <div>
+              <Fade in={isVisible} timeout={2000}>
+                <a href="/files/Currículo - João Vitor.pdf" download>
+                  <button>
+                    Currículo
+                    <DownloadSimple size={16} weight="bold" />
+                  </button>
+                </a>
+              </Fade>
+            </div>
+          </S.AboutMeSkills>
+        </aside>
       </S.AboutMeMainCard>
     </S.AboutMeContainer>
   )
--- src/components/aboutMe/styles.ts
+++ src/components/aboutMe/styles.ts
@@ -7,6 +7,8 @@ export const AboutMeContainer = styled.div`
   align-items: center;
   gap: 2rem;
   margin: 0 8rem;
+  max-width: 100%;
+  overflow: hidden;
   @media (max-width: 1460px) {
     margin: 0;
   }
@@ -18,7 +20,7 @@ export const AboutMeContainer = styled.div`
 export const AboutMeMainCard = styled.main`
   width: 100%;
   display: flex;
-  gap: 1rem;
+  gap: 2rem;
   justify-content: center;
   align-items: center;
   @media (max-width: 862px) {
@@ -32,15 +34,68 @@ export const AboutMeMainCard = styled.main`
   }
   img {
     width: 22rem;
+    height: 22rem;
     border-radius: 10px;
+    box-shadow: 3px 3px 0 0 rgba(0, 0, 0, 0.5);
   }
-
-  p {
-    font-size: 1rem;
-    font-weight: 500;
+  aside {
     width: 100%;
-    font-family: system-ui, sans-serif;
-    color: white;
-    line-height: 1.8;
+    height: 100%;
+    row-gap: 1rem;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    p {
+      font-size: 1rem;
+      font-weight: 500;
+      width: 90%;
+      font-family: system-ui, sans-serif;
+      color: white;
+      line-height: 1.8;
+    }
+  }
+`
+
+export const AboutMeSkills = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 1rem;
+  width: 90%;
+
+  img {
+    background-color: transparent;
+    width: 3.75rem;
+    height: 3.75rem;
+    box-shadow: none;
+  }
+  div {
+    flex: 1;
+    justify-content: flex-end;
+    align-items: flex-end;
+    display: flex;
+    padding-right: 1rem;
+
+    a {
+      text-decoration: none;
+    }
+    button {
+      background: transparent;
+      border: 1px solid ${(props) => props.theme['base-text-color']};
+      color: white;
+      font-family: system-ui, sans-serif;
+      font-weight: 500;
+      gap: 0.5rem;
+      display: flex;
+      align-items: center;
+      height: 2rem;
+      padding: 0 1.3rem;
+      border-radius: 15px;
+      &:hover {
+        cursor: pointer;
+        transform: scale(1.03);
+        transition: transform 0.3s ease-in-out;
+        border: 1px solid ${(props) => props.theme['second-text-color']};
+      }
+    }
   }
 `
